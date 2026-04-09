cd12fd70353dafaf5294afee2ea5a434f4e2f024
Yashvivanza <yashvivanza01@gmail.com>
2026-04-09 16:05:49 +0530

    Initial commit

---
+++ flutter_application_52/lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(title: 'Animals'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: ListView(
+        scrollDirection: Axis.horizontal,
+        children: const[
+          Text("🐘 Elephant"),
+          Text("🦁 Lion"),
+          Text("🦒 Jiraffe"),
+          Text("🐱 Cat"),
+          Text("🐶 Dog"),
+          Text("🐼 Panda"),
+          
+        ],
+      ),
+    );
+  }
+}
