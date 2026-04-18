55b637bcbab7356cc9a6f4644dff2d453074cc7b
Yashvivanza <yashvivanza01@gmail.com>
2026-04-18 13:06:09 +0530

    AudioPlayer

---
+++ flutter_application_66/lib/main.dart
@@ -0,0 +1,56 @@
+
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(title: 'MyApp'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String myAudioFilePath = "audio/song.mp3";
+  AudioPlayer audioPlayer = AudioPlayer();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            ElevatedButton(onPressed: () async {
+              await audioPlayer.play(AssetSource(myAudioFilePath));
+              print("Audio Play");
+            }, 
+            child: Text("Play")),
+          ],
+        ),
+      ),
+    );
+  }
+}
