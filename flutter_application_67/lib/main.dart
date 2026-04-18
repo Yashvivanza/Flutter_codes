b24e62741ce8c51f43c7c3bbbe8c8c3d5bf23b50
Yashvivanza <yashvivanza01@gmail.com>
2026-04-18 13:25:13 +0530

    Audioplayer withh all 4 buttons

---
+++ flutter_application_67/lib/main.dart
@@ -0,0 +1,78 @@
+
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(title: 'MyApp'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String myAudioFilePath = "audio/song.mp3";
+  AudioPlayer audioPlayer = AudioPlayer();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            // Play
+            ElevatedButton(onPressed: () async {
+              await audioPlayer.play(AssetSource(myAudioFilePath));
+              print("Audio Play");
+            }, 
+            child: const Text("Play")),
+
+            // Pause
+            ElevatedButton(onPressed: () async {
+              await audioPlayer.pause();
+              print("Audio pause");
+            }, 
+            child: const Text("Pause")),
+
+            // Resume()
+            ElevatedButton(onPressed: () async {
+              await audioPlayer.resume();
+              print("Audio resume");
+            }, 
+            child: const Text("Resume")),
+
+            // Stop
+            ElevatedButton(onPressed: () async {
+              await audioPlayer.stop();
+              print("Audio Stop");
+            }, 
+            child: const Text("Stop")),
+          ],
+        ),
+      ),
+    );
+  }
+}
