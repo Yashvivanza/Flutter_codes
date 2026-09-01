32af5c838f1d5eec477c823489d15b91f7debc95
Yashvi Vanza <yashvivanza01@gmail.com>
2026-09-01 09:59:22 +0530

    Initial commit

---
+++ flutter_application_105/android/app/src/main/kotlin/com/example/flutter_application_105/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_application_105
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ flutter_application_105/lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.deepPurple[200],
+        appBar: AppBar(
+          title:Center(
+            child:Text("My App Bar",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+              ),
+            ),
+          ),
+          backgroundColor: Colors.deepPurple,
+          elevation: 0,
+        
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          color: Colors.white,
+          onPressed: () {
+            // Handle menu button press
+          },
+        ),
+        actions:[
+          IconButton(
+            onPressed: (){}, 
+            icon: Icon(Icons.logout),
+            color: Colors.white)
+        ]
+        ),
+        body: Center(
+          child: Container(
+            height: 300,
+            width: 300,
+            decoration: BoxDecoration(
+              color: Colors.deepPurple,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            padding: EdgeInsets.all(25),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Hello, World!",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
+                ),
+                Icon(
+                  Icons.favorite,
+                  color: Colors.white,
+                  size: 50,
+                ),
+              ],
+            ),
+          ),
+        )
+      ),
+    );
+  }
+}
